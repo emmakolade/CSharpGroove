9fd785f54f276d819c6eccc55813a54e509004a7
Akolade <emmakolade@gmail.com>
2024-05-26 21:33:45 +0100

    feat: no result

--- Program.cs
+++ Program.cs
@@ -767,7 +767,6 @@ Console.WriteLine("Enter a string containing at least three characters:");
 int numericValue = 0;
 bool validNumber = false;
 
-
 do{
     readResult = Console.ReadLine();
     validNumber = int.TryParse(readResult, out numericValue);
