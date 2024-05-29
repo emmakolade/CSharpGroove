4b02d48769a5f972e8addf64a839b5b10aa70f50
Akolade <emmakolade@gmail.com>
2024-05-29 08:48:09 +0100

    feat:floating points data types

--- Program.cs
+++ Program.cs
@@ -965,7 +965,7 @@ Console.WriteLine("this is a signed integral types");
 Console.WriteLine($"sbyte   : {sbyte.MinValue} to {sbyte.MaxValue}");
 Console.WriteLine($"short   : {short.MinValue} to {short.MaxValue}");
 Console.WriteLine($"int     : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}");
+Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}\n");
 
 
 
@@ -974,4 +974,11 @@ Console.WriteLine("this is an unsigned integral types");
 Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
 Console.WriteLine($"ushort   : {ushort.MinValue} to {ushort.MaxValue}");
 Console.WriteLine($"uint     : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong    : {ulong.MinValue} to {ulong.MaxValue}");
+Console.WriteLine($"ulong    : {ulong.MinValue} to {ulong.MaxValue}\n");
+
+
+Console.WriteLine("this is a floating point types");
+
+Console.WriteLine($"float    : {float.MinValue}  to {float.MaxValue}");
+Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue}");
+Console.WriteLine($"decimal  : {decimal.MinValue}   to {decimal.MaxValue}");
