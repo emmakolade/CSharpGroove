c561721be358728ef4fe4a0b6ada7741ad0948a3
Akolade <emmakolade@gmail.com>
2024-05-30 21:23:45 +0100

    casting operator ()

--- Program.cs
+++ Program.cs
@@ -1016,8 +1016,13 @@ using System.Security.Cryptography;
 
 
 
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
-int myInt = (int) myDecimal;
-Console.WriteLine($"int: {myInt}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+// int myInt = (int) myDecimal;
+// Console.WriteLine($"int: {myInt}");
 
+decimal myDecimal  = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float: {myFloat}");
