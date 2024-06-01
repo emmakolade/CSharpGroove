b1d06f6acea9d6f310345b6a26808f76763b58d9
Akolade <emmakolade@gmail.com>
2024-06-01 05:31:21 +0100

    resizing the array

--- Program.cs
+++ Program.cs
@@ -1111,10 +1111,18 @@ Console.WriteLine($"Float: {myFloat}");
 string[] pallets = ["B14", "A11", "B12", "A13"];
 Console.WriteLine("");
 
-Console.WriteLine($"Before: {pallets[0]}");
 Array.Clear(pallets, 0, 2);
-Console.WriteLine($"After: {pallets[0]}");
-Console.WriteLine($"Clearing two Pallets..... count:{pallets.Length}");
+Console.WriteLine($"Clearing 2 Pallets..... count:{pallets.Length} pallets");
 foreach(var pallet in pallets){
     Console.WriteLine($"-- {pallet}");
+}
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach(var pallet in pallets){
+    Console.WriteLine($"{pallet}");
 }
