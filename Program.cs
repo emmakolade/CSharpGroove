f90c2206231c1d8a8b84fbb156460532e73a628a
Akolade <emmakolade@gmail.com>
2024-06-08 19:24:51 +0100

    methos to structure code

--- Program.cs
+++ Program.cs
@@ -1683,79 +1683,101 @@ of profgramming languge resources
 // DisplayRandomNumbers();
 
 
-int[] times = [800, 1200, 1600, 2000];
-int diff = 0;
+// int[] times = [800, 1200, 1600, 2000];
+// int diff = 0;
 
 
 
-Console.WriteLine("Enter current GMT");
-int currentGMT = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter current GMT");
+// int currentGMT = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Current Medicine Schedule:");
+// Console.WriteLine("Current Medicine Schedule:");
 
-/* Format and display medicine times */
-DisplayTimes();
-Console.WriteLine();
+// /* Format and display medicine times */
+// DisplayTimes();
+// Console.WriteLine();
 
-Console.WriteLine("Enter new GMT");
-int newGMT = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter new GMT");
+// int newGMT = Convert.ToInt32(Console.ReadLine());
 
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-{
-    Console.WriteLine("Invalid GMT");
+// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+// {
+//     Console.WriteLine("Invalid GMT");
+// }
+// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+// {
+//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+
+//     AdjustTImes();
+// }
+// else
+// {
+//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+
+//     AdjustTImes();
+// }
+
+// Console.WriteLine("New Medicine Schedule:");
+
+// DisplayTimes();
+
+// Console.WriteLine();
+
+// void DisplayTimes()
+// {
+//     /* Format and display medicine times */
+
+//     foreach (int val in times)
+//     {
+//         string time = val.ToString();
+//         int len = time.Length;
+
+//         if (len >= 3)
+//         {
+//             time = time.Insert(len - 2, ":");
+
+//         }
+//         else if (len == 2)
+//         {
+//             time = time.Insert(0, "0:");
+//         }
+//         else
+//         {
+//             time = time.Insert(0, "0:0");
+//         }
+//         Console.WriteLine($"{time}");
+//     }
+//     Console.WriteLine();
+// }
+
+// void AdjustTImes()
+// {
+//     /* Adjust the times by adding the difference, keeping the value within 24 hours */
+
+//     for (int i = 0; i < times.Length; i++)
+//     {
+//         times[i] = ((times[i] + diff)) % 2400;
+//     }
+// }
+
+string ipv4Input = "107.31.1.5";
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+void ValidateLength(){
+    string[] address = ipv4Input.Split(".");
+    validLength = address.Length == 4;
 }
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-{
-    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
 
-    AdjustTImes();
-}
-else
-{
-    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-
-    AdjustTImes();
-}
-
-Console.WriteLine("New Medicine Schedule:");
-
-DisplayTimes();
-
-Console.WriteLine();
-
-void DisplayTimes()
-{
-    /* Format and display medicine times */
-
-    foreach (int val in times)
+void ValidateZeroes(){
+    string[] address = ipv4Input.Split(".");
+    foreach (string num in address)
     {
-        string time = val.ToString();
-        int len = time.Length;
-
-        if (len >= 3)
-        {
-            time = time.Insert(len - 2, ":");
-
+        if (num.Length > 1 && num.StartsWith("0")){
+            validZeroes = false;
         }
-        else if (len == 2)
-        {
-            time = time.Insert(0, "0:");
-        }
-        else
-        {
-            time = time.Insert(0, "0:0");
-        }
-        Console.WriteLine($"{time}");
     }
-    Console.WriteLine();
+    validZeroes =true;
 }
 
-void AdjustTImes()
-{
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-
-    for (int i = 0; i < times.Length; i++)
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
-}
--- obj/Debug/net8.0/CSharpGroove.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharpGroove.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpGroove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997cd9f6ad1a6865ee7e2e82d0739135f44b96cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26948ae267b9476ba4afa8088e219f6c1e00a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpGroove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpGroove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
