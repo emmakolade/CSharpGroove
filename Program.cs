e2f99a605f4ca489b846a2a8da78b6ad78f2b8bc
Akolade <emmakolade@gmail.com>
2024-06-09 16:32:29 +0100

    methos to structure code

--- Program.cs
+++ Program.cs
@@ -1759,25 +1759,59 @@ of profgramming languge resources
 //         times[i] = ((times[i] + diff)) % 2400;
 //     }
 // }
-
-string ipv4Input = "107.31.1.5";
+string[] ipv4Input = ["107.31.1.5", "255.0.0.255", "555..0.555", "255...255"];
+string[] address;
 bool validLength = false;
 bool validZeroes = false;
 bool validRange = false;
 
-void ValidateLength(){
-    string[] address = ipv4Input.Split(".");
-    validLength = address.Length == 4;
+foreach (string ip in ipv4Input) 
+{
+    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+    ValidateLength(); 
+    ValidateZeroes(); 
+    ValidateRange();
+
+    if (validLength && validZeroes && validRange) 
+    {
+        Console.WriteLine($"{ip} is a valid IPv4 address");
+    } 
+    else 
+    {
+        Console.WriteLine($"{ip} is an invalid IPv4 address");
+    }
 }
 
-void ValidateZeroes(){
-    string[] address = ipv4Input.Split(".");
-    foreach (string num in address)
+void ValidateLength() 
+{
+    validLength = address.Length == 4;
+};
+
+void ValidateZeroes() 
+{
+    foreach (string number in address) 
     {
-        if (num.Length > 1 && num.StartsWith("0")){
+        if (number.Length > 1 && number.StartsWith("0")) 
+        {
             validZeroes = false;
+            return;
         }
     }
-    validZeroes =true;
+
+    validZeroes = true;
 }
 
+void ValidateRange() 
+{
+    foreach (string number in address) 
+    {
+        int value = int.Parse(number);
+        if (value < 0 || value > 255) 
+        {
+            validRange = false;
+            return;
+        }
+    }
+    validRange = true;
+}
