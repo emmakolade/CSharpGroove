d0ded02a3dcf53374c841a45283740a018a53501
Akolade <emmakolade@gmail.com>
2024-06-09 16:48:25 +0100

    feat: add parameters to methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 using System.Globalization;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
@@ -1817,25 +1818,35 @@ of profgramming languge resources
 // }
 
 
-Random random = new();
-int luck = random.Next(100);
+// Random random = new();
+// int luck = random.Next(100);
 
-string[] text = ["You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"];
-string[] good = ["look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"];
-string[] bad = ["fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."];
-string[] neutral = ["appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."];
+// string[] text = ["You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"];
+// string[] good = ["look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"];
+// string[] bad = ["fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."];
+// string[] neutral = ["appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."];
 
-TellFortune();
-luck = random.Next(100);
-TellFortune();
-void TellFortune()
-{
+// TellFortune();
+// luck = random.Next(100);
+// TellFortune();
+// void TellFortune()
+// {
 
-    Console.WriteLine("A fortune teller whispers the following words:");
-    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-    for (int i = 0; i < 4; i++)
+//     Console.WriteLine("A fortune teller whispers the following words:");
+//     string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+//     for (int i = 0; i < 4; i++)
+//     {
+//         Console.Write($"{text[i]} {fortune[i]} ");
+//     }
+
+// }
+
+// Add Parameters to Methods 
+CountTo(5);
+
+void CountTo(int maxNumber){
+    for (int i = 0; i < maxNumber; i++)
     {
-        Console.Write($"{text[i]} {fortune[i]} ");
+        Console.WriteLine($"{i}, ");
     }
-
 }
